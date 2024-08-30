1d4af04a0cc2cb6586086ea44cb625934d75de1c
IPF . <nazagarcia132@gmail.com>
2024-08-30 15:51:23 -0300

    primer commit

---
+++ src/main.js
@@ -0,0 +1,18 @@
+import './style.css'
+
+import { Header } from './components/header';
+import { LandingPage } from './pages/landing';
+import { LoginPage } from './pages/login';
+
+
+const pathname = window.location.pathname;
+
+const $app = document.querySelector("#app");
+
+$app.appendChild(Header());
+
+if (pathname === "/") $app.appendChild(LandingPage());
+if (pathname === "/login") $app.appendChild(LoginPage());
+
+if (pathname === "/about") $app.appendChild(LandingPage());
+if (pathname === "/contact") $app.appendChild(LoginPage());
